2177fdace99cbf96989673ec51542b83734a6485
Rapptz <rapptz@gmail.com>
2014-08-26 04:41:09 -0400

    Document sdl::error. Clear the error after receiving it.

--- sdlpp/core/error.hpp
+++ sdlpp/core/error.hpp
@@ -24,9 +24,22 @@
 #include <stdexcept>
 
 namespace sdl {
+/**
+ * @brief Exception object that handles SDL errors.
+ * @details The exception object either propagates
+ * `SDL_GetError` or uses a custom string. All error
+ * handling is thrown through this object.
+ */
 class error : public std::runtime_error {
 public:
-    error() noexcept: std::runtime_error(SDL_GetError()) {}
+    /**
+     * @brief Creates an exception with `SDL_GetError`
+     */
+    error() noexcept: std::runtime_error(SDL_GetError()) { SDL_ClearError(); }
+
+    /**
+     * @brief Creates an exception with a custom string.
+     */
     error(const std::string& str) noexcept: std::runtime_error(str) {}
 };
 } // sdl
